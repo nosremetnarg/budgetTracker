767d9fef0fb45fce0c339c0d33f76c0127cdcc21
nosremetnarg <grant.alex.emerson@gmail.com>
2020-10-09 18:00:58 -0500

    deploying

--- server.js
+++ server.js
@@ -16,9 +16,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(MONGODB_URI || 'mongodb://localhost/budgetTracker', {
+  useFindAndModify: false,
   useNewUrlParser: true,
-  useFindAndModify: false
+  useUnifiedTopology: true
 });
 
 // routes
