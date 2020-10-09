8d33fcccb5d1f829d226d4161359e05fc9e071b1
nosremetnarg <grant.alex.emerson@gmail.com>
2020-10-09 17:02:53 -0500

    preparing to deploy to heroku

--- public/js/idb.js
+++ public/js/idb.js
@@ -21,7 +21,7 @@ request.onsuccess = function(event) {
     // check if app is online, if yes run uploadPizza() function to send all local db data to api
     if (navigator.onLine) {
       // we haven't created this yet, but we will soon, so let's comment it out for now
-      // uploadPizza();
+      uploadTransaction();
     }
   };
   
@@ -41,4 +41,50 @@ function saveRecord(record) {
     // add record to your store with add method
     budgetObjectStore.add(record);
   }
+
+  function uploadTransaction() {
+    // open a transaction on your db
+    const transaction = db.transaction(['new_budget'], 'readwrite');
+
+    // access your object store
+    const budgetObjectStore = transaction.objectStore('new_budget');
+
+    // get all records from store and set to a variable
+    const getAll = budgetObjectStore.getAll(); // getAll needs an event listener
+
+    // upon a successful .getAll() execution, run this function
+    getAll.onsuccess = function () {
+        // if there was data in indexedDb's store, let's send it to the api server
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+                    // open one more transaction
+                    const transaction = db.transaction(['new_budget'], 'readwrite');
+                    // access the new_pizza object store
+                    const budgetObjectStore = transaction.objectStore('new_budget');
+                    // clear all items in your store
+                    budgetObjectStore.clear();
+
+                    alert('All saved transaction has been submitted!');
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    }
+};
+
+// listen for app coming back online
+window.addEventListener('online', uploadTransaction);
   
